463c9d55e464534a72c91d90230db8a96f6d261a
Sandeepdevops0412 <sandeepdevops0412@gmail.com>
2021-02-18 02:28:04 +0530

    second commit

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
+        body: JSON.stringify('Hello from Sandeep'),
     };
     return response;
 };
+
