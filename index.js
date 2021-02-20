f277ef4c402b62ad329382db69732bb1941b1731
Sandeepdevops0412 <sandeepdevops0412@gmail.com>
2021-02-21 00:16:43 +0530

    eight commit

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-<<<<<<< HEAD
-        body: JSON.stringify('Hello from keerthi'),
->>>>>>> 3739cc511a20030678d09e6d9aa1aef4187ca927
+        body: JSON.stringify('Hello from Sandeep'),
     };
     return response;
 };
-
